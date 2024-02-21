6c2bd28030d020a0aa5e182cb922abbffe4f5a35
OsintcevIlya <orkaang@gmail.com>
2024-02-21 16:15:42 +0300

    123123123

--- NewMainMyModul/src/main/java/ru/mts/OsintcevIlya/MyProject01/Main.java
+++ NewMainMyModul/src/main/java/ru/mts/OsintcevIlya/MyProject01/Main.java
@@ -10,6 +10,7 @@ public class Main {
         for(int i = 0; i <= 2; i++) {
             //System.out.println("i= "+ i);
             System.out.println("Hello world! In English. i= " + i);
+            System.out.println("Hello world! In English. i= ");
         }
     }
 }
